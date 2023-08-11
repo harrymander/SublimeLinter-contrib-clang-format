e6075f08827e3c2b3eb800a45929498533bbd124
Harry Mander <harrymander96@gmail.com>
2023-08-11 17:51:58 +1200

    Get basic linting working

--- linter.py
+++ linter.py
@@ -1,10 +1,21 @@
-from SublimeLinter.lint import Linter
+import re
+
+from SublimeLinter import lint
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class ClangFormat(lint.Linter):
+    cmd = 'clang-format --dry-run --style=file ${args} ${file}'
+    default_type = lint.WARNING
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.c - source.c++'
     }
+
+    error_stream = lint.STREAM_STDERR
+    multiline = True
+    re_flags = re.MULTILINE
+    regex = (
+        r'^.*?:(?P<line>\d+):(?P<col>\d+): '  # line and column number
+        r'(?:(?P<error>error)|(?P<warning>warning)): '  # 'error' or 'warning'
+        r'(?P<message>.*?)'  # message
+        r'\n.*\n.*$'  # two extra lines to ignore
+    )
